3705ddd5af2afe8f25c5503c8a2b16366d4c01fd
Nikita Rukin <n.rukin@gmail.com>
2024-01-15 13:21:50 +0300

    Получение сообщений и обновлений сообщений

--- main.go
+++ main.go
@@ -17,4 +17,25 @@ func main() {
 		log.Panic(err)
 	}
 	log.Printf("Authorised on account %s", bot.Self.UserName)
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+
+		var msg *tgbotapi.Message
+		switch {
+		case update.Message != nil:
+			msg = update.Message
+		case update.EditedMessage != nil:
+			msg = update.EditedMessage
+		default:
+			continue
+		}
+
+	}
 }
