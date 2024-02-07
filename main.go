8e50edbe55e210618b3496bded1c58d35936e2c9
Nikita Rukin <n.rukin@gmail.com>
2024-02-07 17:17:35 +0300

    Продолжение: Получение позиции и треки

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+type Location struct {
+	Track     string
+	Latitude  float64
+	Longitude float64
+	Timestamp int
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -17,4 +26,60 @@ func main() {
 		log.Panic(err)
 	}
 	log.Printf("Authorised on account %s", bot.Self.UserName)
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+
+		var msg *tgbotapi.Message
+		switch {
+
+		case update.Message != nil:
+			msg = update.Message
+		case update.EditedMessage != nil:
+			msg = update.EditedMessage
+		default:
+			continue
+		}
+
+		loc, err := getLocationFromMessage(msg)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Printf("%+v", loc)
+
+	}
+}
+
+func getLocationFromMessage(msg *tgbotapi.Message) (Location, error) {
+
+	if msg.Location == nil {
+		return Location{}, errors.New("Msg has no location")
+	}
+
+	tid := getTrackFromMessage(msg)
+
+	loc := Location{
+		Track:     tid,
+		Latitude:  msg.Location.Latitude,
+		Longitude: msg.Location.Longitude,
+		Timestamp: msg.Date,
+	}
+
+	if msg.EditDate != 0 {
+		loc.Timestamp = msg.EditDate
+	}
+
+	return loc, nil
+
+}
+
+func getTrackFromMessage(msg *tgbotapi.Message) string {
+	return fmt.Sprintf("%d_%d", msg.Chat.ID, msg.MessageID)
 }
